9c80410d58850903f58dd0dc43adfcd31d08631f
mzlatic <mzlatic@tvz.hr>
2019-10-31 22:46:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <button class="button is-primary">This button</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+
+}
+</script>
+
+<style lang="scss">
+  @import "~bulma/css/bulma.css";
+</style>
